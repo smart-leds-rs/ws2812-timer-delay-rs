7c8aedf174a2a84daf13e47aa72aea2deb907751
Jacob <jacobrosenthal@gmail.com>
2019-11-19 12:45:51 -0700

    document ws2812 timings attempting to acheive, bring ws2812 inter bit timing up above minimum, delay after sending a byte not before, update to v2 traits

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,19 @@
 //! If it's too slow (e.g.  e.g. all/some leds are white or display the wrong color)
 //! you may want to try the `slow` feature.
 
+// https://wp.josh.com/2014/05/13/ws2812-neopixels-are-not-so-finicky-once-you-get-to-know-them/
+//                                          Min     Typ     Max     Units
+// T0H      0 code  high voltage time       200     350     500     ns
+// T1H      1 code  high voltage time       550     700     5,500   ns
+// TLD      data    low voltage time        450     600     5,000   ns
+// TLL      latch   low voltage time        6,000                   ns
+
+// The tricky timing is the T0H between 200 and 350ns timing.
+// 3mhz is 333ns per block and should be flexible for all other timings
+
+// But new device reset latch timing is posted as 280us
+// https://blog.particle.io/2017/05/11/heads-up-ws2812b-neopixels-are-about-to-change/
+
 #![no_std]
 
 use embedded_hal as hal;
@@ -61,16 +74,18 @@ where
     fn write_byte(&mut self, mut data: u8) {
         for _ in 0..8 {
             if (data & 0x80) != 0 {
-                block!(self.timer.wait()).ok();
+                // 1bit, 666ns on time, 666ns off time
                 self.pin.set_high().ok();
                 block!(self.timer.wait()).ok();
                 block!(self.timer.wait()).ok();
                 self.pin.set_low().ok();
+                block!(self.timer.wait()).ok();
             } else {
-                block!(self.timer.wait()).ok();
+                // 0bit, 333ns on time, 666ns off time
                 self.pin.set_high().ok();
                 block!(self.timer.wait()).ok();
                 self.pin.set_low().ok();
+                block!(self.timer.wait()).ok();
                 block!(self.timer.wait()).ok();
             }
             data <<= 1;
@@ -97,9 +112,11 @@ where
             self.write_byte(item.r);
             self.write_byte(item.b);
         }
-        // Get a timeout period of 300 ns
+
+        // Latch for > 280 us
+        // 900 * 333 = 299 us
         for _ in 0..900 {
-            block!(self.timer.wait()).ok();
+            let _ = block!(self.timer.wait());
         }
         Ok(())
     }
